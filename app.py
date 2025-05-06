5f120754e50699904a713e25c3007d38ca9d6c34
Sambi <sambisingh289@gmail.com>
2025-05-05 20:38:43 -0500

    First api project done! :)

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ city = input("Enter city: ")
 location_data = requests.get(
     f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={api_key}")
 
-print(location_data.json())
+
+#print(location_data.json())
+lon = location_data.json() [0]['lon']
+lat = location_data.json() [0]['lat']
+#print(lat)
+#print(lon)
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}")
 #print(weather_data.status_code)
+#print(weather_data.json())
+weather = weather_data.json()['weather'][0]['main']
+print(f"The weather in {city} right now is {weather}")
